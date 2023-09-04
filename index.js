41e7ade505db391b80bd95f26862be8dcac0c2b7
Madhusha <madhushasampath@gmail.com>
2023-09-05 03:22:39 +0530

    child class create

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
+//Parent class
 class User {
   constructor(name, age, gender) {
     this.name = name;
     this.age = age;
     this.gender = gender;
   }
+
+  showDetails = () => {
+    console.log(`Name: ${this.name}`);
+    console.log(`Age: ${this.age}`);
+    console.log(`Gender: ${this.gender}`);
+  };
 }
+
+//Child class
+class Bank extends User {
+  constructor(name, age, gender) {
+    super(name, age, gender);
+    this.balance = 0;
+  }
+
+  deposit = (amount) => {
+    this.amount = amount;
+    this.balance = this.balance + this.amount;
+    console.log(
+      `\nDeposit: ${this.amount} \nAccount Name: ${this.name} \nAccount balance has been updated: LKR.${this.balance}\n `
+    );
+  };
+
+  withdraw = (amount) => {
+    this.amount = amount;
+    let withdrawalFee = 5;
+    if (this.amount > this.balance) {
+      console.log(
+        `Withdraw: ${this.amount} \nInsufficient balance !! \nBalance avilable: ${this.balance}\n`
+      );
+    } else {
+      this.balance = this.balance - (this.amount + withdrawalFee);
+      console.log(
+        `Withdraw: ${this.amount} \nAccount Name: ${this.name} \nAccount balance has been updated: LKR.${this.balance}\n`
+      );
+    }
+  };
+
+  viewBalance = () => {
+    console.log("Personal details");
+    this.showDetails();
+    console.log(`Account Balance Available: ${this.balance}`);
+  };
+}
+
+const John = new Bank("John", 33, "male");
+John.deposit(1000);
+John.withdraw(500);
+John.withdraw(500);
+John.viewBalance();
+
+const Emily = new Bank("Emily", 27, "female");
+Emily.deposit(500);
+Emily.withdraw(300);
+Emily.viewBalance();
